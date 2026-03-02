820b1a88a925a7825f3d2af2c41ad4d0685922b3
xzhou25 <xzhou25@gmail.com>
2026-03-02 14:17:46 +0800

    remove useless code

--- src/main/resources/db/data.sql
+++
@@ -1,7 +0,0 @@
--- 初始化数据
-
--- 插入测试用户(密码: 123456)
--- SHA256: 8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
-INSERT INTO users (username, email, password, create_time, update_time, is_deleted)
-VALUES ('admin', 'admin@example.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0)
-ON CONFLICT (username) DO NOTHING;
